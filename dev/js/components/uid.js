e805ded76a462450594e1010ecdb2f2e495cd999
zax456 <kianhwee1994@hotmail.com>
2018-03-24 18:01:28 +0800

    added uid page

---
+++ dev/js/components/uid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './login'
+import { TextField } from 'material-ui';
+
+const Uid = () => (
+    <div>
+        <textarea></textarea>
+        <button>Enter</button>
+    </div>
+);
+
+export default Uid;
