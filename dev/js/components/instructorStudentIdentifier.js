ea7d6c444b5dcefabbdf40314842edc487edefdc
Markus Ng <e0014936@u.nus.edu>
2018-04-06 11:57:50 +0800

    UI style for instructorStudentIdentifier

--- dev/js/components/instructorStudentIdentifier.js
+++ dev/js/components/instructorStudentIdentifier.js
@@ -10,6 +10,8 @@ import RechartsComp from './RechartsChart.js';
 import Grid from 'material-ui/Grid';
 import store from '../store';
 import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+
 
 import {
     PieChart,
@@ -21,7 +23,9 @@ import {
     CartesianGrid,
     Tooltip,
     Legend,
-    ResponsiveContainer
+    ResponsiveContainer,
+    ReferenceLine,
+    Cell
   } from "recharts";
 import { BarChart, Bar } from "recharts";
 const AxisLabel = ({
@@ -49,44 +53,70 @@ const AxisLabel = ({
       </text>
     );
   };
+var divStyle = {
+    padding: "1px"
+  };
 
 class instructorStudentIdentifier extends React.Component {
+
+    topStudentSubmissions(data){
+        var result = []
+        // console.log('topStudentSubmissions')
+        // console.log(data)
+        for(var i in data){
+            // console.log(data[i])
+            result.push(data[i].value)
+        }
+        var totalStudent = result.length
+        result.sort(function(a, b){return b-a})
+        // console.log(result[Math.round(totalStudent/4)])
+        return(result[Math.round(totalStudent/4)])
+    }
+
     render() {
         const state = store.getState();
         return (
             <div>
-                <Grid container spacing={8}>
-                <Grid item xs={6}>
+                <Grid container spacing={8} direction="row" align="center">
+                <Grid item xs={12}>
                 <Paper>
-                <h3>Chart12</h3>
-                <h4>Which Students Show a High/Low Rate of Assignment Submission?</h4>
-                <ResponsiveContainer width="90%" height={400}>
+                <div style={divStyle}>
+                <h3>Student Submissions</h3>
+                <h4>Monitor Total Submission of Each Student</h4>
+                </div>
+                <Divider light />
+                <ResponsiveContainer width="90%" height={380}>
                 <BarChart
-                    width={730}
-                    height={250}
+                    // width={730}
+                    // height={250}
                     data={this.props.firebase.val[this.props.activeProfile.uid][this.props.activeProfile.course].instructorStudentIdentifier.chart12.data}
                     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
                     >
                     <XAxis
                     dataKey="student_name"
-                    label={
-                        <AxisLabel axisType="xAxis" width={600} height={300}>
-                        Student
-                        </AxisLabel>
-                    }
+                    label={{
+                        value:"Student",
+                        position: "insideBottom"
+                    }}
+                    ticks={[0]}
                     />
                     <YAxis
                     dataKey="value"
                     label={
-                        <AxisLabel axisType="yAxis" width={600} height={300}>
+                        <AxisLabel width={40} height={380}axisType="yAxis">
                         Number of Submissions
                         </AxisLabel>
                     }
                     />
                     <CartesianGrid strokeDasharray="3 3" />
                     <Tooltip />
-                    <Legend />
-                    <Bar dataKey="value" fill="#8884d8"/>
+                    <Bar dataKey="value" fill="#3498DB">
+                    onClick={(data, index) => this.selectedAssignment(data.assignment)}>
+                        {this.props.firebase.val[this.props.activeProfile.uid][this.props.activeProfile.course].instructorStudentIdentifier.chart12.data.map((entry, index) => (
+                            <Cell key={entry.student_name} fill={entry.value < 20 ? '#d68995' : '#71afe2' }/>
+                        ))}
+                    </ Bar>
+                    <ReferenceLine strokeDasharray="3 3" y={20} strokeWidth={4} stroke="#e0b13c" label={{value: "Expectation", position: "insideTop"}} />
                 </BarChart>
                 </ ResponsiveContainer>
                 </Paper>
@@ -94,71 +124,89 @@ class instructorStudentIdentifier extends React.Component {
                 
                 <Grid item xs={6}>
                 <Paper>
-                <h3>Chart13</h3>
-                <h4>Which Students Appear To Be High-Achieving/Do More Than What Is Required?</h4>
-                <ResponsiveContainer width="90%" height={400}>
+                <div style={divStyle}>
+                <h3> Progress of Latest Assignment </h3>
+                <h4>Identify Possible Achievers</h4>
+                </div>
+                <Divider light />
+                <ResponsiveContainer width="90%" height={380}>
                 <BarChart
-                    width={730}
-                    height={250}
+                    // width={730}
+                    // height={250}
                     data={this.props.firebase.val[this.props.activeProfile.uid][this.props.activeProfile.course].instructorStudentIdentifier.chart13.data}
                     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
                     >
                     <XAxis
                     dataKey="student_name"
-                    label={
-                        <AxisLabel axisType="xAxis" width={600} height={300}>
-                        Student
-                        </AxisLabel>
+                    label={{
+                        value:"Student",
+                        position: "insideBottom",
                     }
+                    }
+                    ticks={[0]}
                     />
                     <YAxis
                     dataKey="value"
                     label={
-                        <AxisLabel axisType="yAxis" width={600} height={300}>
+                        <AxisLabel width={40} height ={380} axisType="yAxis">
                         Number of Subsmission
                         </AxisLabel>
                     }
                     />
                     <CartesianGrid strokeDasharray="3 3" />
                     <Tooltip />
-                    <Legend />
-                    <Bar dataKey="value" fill="#8884d8" />
+                    <Bar dataKey="value" fill="#66CDAA">
+                    onClick={(data, index) => this.selectedAssignment(data.assignment)}>
+                        {this.props.firebase.val[this.props.activeProfile.uid][this.props.activeProfile.course].instructorStudentIdentifier.chart13.data.map((entry, index) => (
+                            <Cell key={entry.student_name} fill={entry.value > this.topStudentSubmissions(this.props.firebase.val[this.props.activeProfile.uid][this.props.activeProfile.course].instructorStudentIdentifier.chart13.data) ? '#66CDAA' : '#3498DB' }/>
+                        ))}
+                    </Bar>
+                    {/* {this.topStudentSubmissions(this.props.firebase.val[this.props.activeProfile.uid][this.props.activeProfile.course].instructorStudentIdentifier.chart13.data)} */}
+                    <ReferenceLine strokeDasharray="3 3" 
+                    y={this.topStudentSubmissions(this.props.firebase.val[this.props.activeProfile.uid][this.props.activeProfile.course].instructorStudentIdentifier.chart13.data)} 
+                    strokeWidth={4} stroke="#e0b13c" label={{value: "75 Percentile", position: "insideTop"}} />
                 </BarChart>
                 </ResponsiveContainer>
                 </Paper>
                 </Grid>
 
-                <Grid item xs={12}>
+                <Grid item xs={6}>
                 <Paper>
-                <h3>Chart14</h3>
-                <h4>Which student is always quick/slow to submit?</h4>
-                <ResponsiveContainer width="90%" height={400}>
+                <div style={divStyle}>
+                <h3>Submission Duration</h3>
+                <h4>Idntify Distribution of Time Taken for Submission</h4>
+                </div>
+                <Divider light />
+                <ResponsiveContainer width="90%" height={380}>
                 <BarChart
-                    width={730}
-                    height={250}
+                    // width={730}
+                    // height={250}
                     data={this.props.firebase.val[this.props.activeProfile.uid][this.props.activeProfile.course].instructorStudentIdentifier.chart14.data}
                     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
                     >
                     <XAxis
                     dataKey="student_name"
-                    label={
-                        <AxisLabel axisType="xAxis" width={600} height={300}>
-                        Student
-                        </AxisLabel>
+                    label={{
+                        value:"Student",
+                        position: "insideBottom",
                     }
+                    }
+                    ticks={[0]}
+                    
                     />
                     <YAxis
                     dataKey="value"
                     label={
-                        <AxisLabel axisType="yAxis" width={600} height={300}>
+                        <AxisLabel width={40} height ={380} axisType="yAxis">
                         Number of Days Taken
                         </AxisLabel>
                     }
                     />
                     <CartesianGrid strokeDasharray="3 3"/>
                     <Tooltip />
-                    <Legend />
-                    <Bar dataKey="value" fill="#8884d8"/>
+                    <Bar dataKey="value" fill="#3498DB">
+
+                    </Bar>
                 </BarChart>
                 </ResponsiveContainer>
                 </Paper> 
